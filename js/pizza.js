767900d87ce376975bb3057599ea14dbc3b06da6
Dylan Tucker <dylantt2000@gmail.com>
2018-02-07 14:32:23 -1000

    1

--- js/pizza.js
+++ js/pizza.js
@@ -4,8 +4,38 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var person = {
+	firstname: "James",
+	lastname: "Franko",
+}
+var address ={	
+	street: "2155 E Lake Dr",
+	city : "Seattle",
+	state: "WA",
+}
+var pizza ={
+	crust: "thin",
+	pizzasauce: "robust marinara",
+	cheese: "regular",
+}
+var	toppings ={
+	Veggies: ["olives", "green pepper"],
+	meats :["pepperoni", "ham","sausage"]
+		}
+	
+var others ={
+	sideItems: ["breadsticks", "bbq wings 6 piece"],
+	sauces: "ranch",
+    instructions:"please cut into squares",
+    payment:"cash",
+    storeId:"112"
 
-
+}
+console.log(person.firstname,person.lastname);
+console.log(address.street,address.city,address.state)
+console.log(pizza.crust,pizza.pizzasauce,pizza.cheese)
+console.log(toppings.Veggies[0],toppings.Veggies[1],toppings.meats[0],toppings.meats[1])
+console.log(others.sideItems[0],others.sideItems[1],others.sauces,others.instructions,others.payment,others.storeId)
 /*
 * Create a new variable named currentOrders
 * currentOrders should be single object containing both orders from above
